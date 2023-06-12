1a8dd326c55804258dcd02beb9eac8431d77d215
juanleonm <jleonm@uc.cl>
2023-06-11 22:41:15 -0400

    termine el recuadro donde iran el spider chart y agrege las descripciones de las habilidades.

--- main.js
+++ main.js
@@ -12,8 +12,11 @@ let g_matriz = SVG1.append('g').attr('id', 'matriz')
 
 let g_stats = SVG1.append('g').attr('id', 'info-agent');
 
-g_stats.append('rect').attr('width', 400).attr('height', 600).attr('fill', 'red')
-let scrollLimit = 1900; // Define tu límite aquí.
+g_stats.append('rect').attr('width', 400).attr('height', 600).attr('fill', '#09253e')
+
+
+
+let scrollLimit = 2000; // límite de scroll para el recuadro de las stats de los agentes.
 
 
 function displayMatrix(data_agents) {
@@ -60,22 +63,117 @@ function displayMatrix(data_agents) {
     })
 
 
-    g_matriz.selectAll('g').on('click', (event, d) => { show_info_agent(d) })
+    g_matriz.selectAll('g').on('click', (event, d) => { 
+
+        // g_matriz.selectAll('g').transition()
+        // .duration(300)
+        // .attr("opacity", 1);
+        
+        
+        show_info_agent(d) 
+        
+    });
 
 }
 
 function show_info_agent(info) {
-    const SVG1 = d3.select('#agents-vis');
+
+    g_matriz.selectAll("g")
+                .attr("opacity", 1);
+
+    
+
+    g_matriz.selectAll("g").filter(infoinfo => infoinfo != info)
+                    .attr("opacity", 0.2);
+    
     console.log(info);
 
-    SVG1.selectAll('g').select('rect').transition().duration(800)
+    g_matriz.selectAll('g').select('rect').transition().duration(800)
         .attr('height', 280).attr('width', 200);
 
     let agent_id = CSS.escape(info.id.toString());
-    let g = SVG1.select(`#${agent_id}`);
+    let g = g_matriz.select(`#${agent_id}`);
     g.raise();
     g.select('rect').transition().duration(2000)
         .attr('height', 330).attr('width', 365)
+    
+    // mostramos las habilidades y ultimate del agente 
+
+    g_stats.selectAll('text').remove();
+   
+    g_stats.append('text').text('STATS AND ABILITIES').attr('x',100).attr('y', 30)
+    .attr('class', 'desc-habilities')
+    .attr('fill', '#156AB7').attr('font-size', 20).attr('font-family', 'sans-serif')
+    .attr('font-weight', 'bold');
+    
+    // mostramos la ultimate del agente con un estilo mas llamativo
+
+    g_stats.append('text').text('Ultimate:' +  info.ulti).attr('x', 100).attr('y', 330)
+        .attr('fill', '#156AB7').attr('font-size', 17).attr('font-family', 'sans-serif');
+        
+    
+    // hacemos los textos de la descripcion sean de 3 lineas    
+
+    let ulti_desc = info.ulti_desc;
+    let ulti_desc1 = ulti_desc.substring(0, 50);
+    let ulti_desc2 = ulti_desc.substring(50, 100);
+    let ulti_desc3 = ulti_desc.substring(100, 150);
+    let ulti_desc4 = ulti_desc.substring(150, 200);
+
+
+    g_stats.append('text').text(ulti_desc1).attr('x', 20).attr('y', 345)
+    g_stats.append('text').text(ulti_desc2).attr('x', 20).attr('y', 360)
+    g_stats.append('text').text(ulti_desc3).attr('x', 20).attr('y', 375)
+    g_stats.append('text').text(ulti_desc4).attr('x', 20).attr('y', 390)
+
+
+
+    
+
+    
+
+    g_stats.append('text').text(info.ability1).attr('x', 20).attr('y', 410)
+        .attr('fill', '#156AB7').attr('font-size', 15).attr('font-family', 'sans-serif')
+
+    // hacemos los textos de la descripcion sean de 3 lineas 
+
+    let ability1_desc = info.ability1_desc;
+    let ability_desc1 = ability1_desc.substring(0, 50);
+    let ability_desc2 = ability1_desc.substring(50, 100);
+    let ability_desc3 = ability1_desc.substring(100, 150);
+
+    g_stats.append('text').text(ability_desc1).attr('x', 20).attr('y', 425)
+    g_stats.append('text').text(ability_desc2).attr('x', 20).attr('y', 440)
+    g_stats.append('text').text(ability_desc3).attr('x', 20).attr('y', 455)
+    
+
+    g_stats.append('text').text(info.ability2).attr('x', 20).attr('y', 475)
+        .attr('fill', '#156AB7').attr('font-size', 15).attr('font-family', 'sans-serif')
+    
+    let ability2_desc = info.ability2_desc;
+    let ability2_desc1 = ability2_desc.substring(0, 50);
+    let ability2_desc2 = ability2_desc.substring(50, 100);
+    let ability2_desc3 = ability2_desc.substring(100, 150);
+    
+    g_stats.append('text').text(ability2_desc1).attr('x', 20).attr('y', 490)
+    g_stats.append('text').text(ability2_desc2).attr('x', 20).attr('y', 505)
+    g_stats.append('text').text(ability2_desc3).attr('x', 20).attr('y', 525)
+
+    g_stats.append('text').text(info.ability3).attr('x', 20).attr('y', 545)
+        .attr('fill', '#156AB7').attr('font-size', 13).attr('font-family', 'sans-serif')
+    
+    let ability3_desc = info.ability3_desc;
+    let ability3_desc1 = ability3_desc.substring(0, 50);
+    let ability3_desc2 = ability3_desc.substring(50, 100);
+    let ability3_desc3 = ability3_desc.substring(100, 150);
+
+    g_stats.append('text').text(ability3_desc1).attr('x', 20).attr('y', 560)
+    g_stats.append('text').text(ability3_desc2).attr('x', 20).attr('y', 575)
+    g_stats.append('text').text(ability3_desc3).attr('x', 20).attr('y', 590)
+   
+
+
+
 
 
 }
@@ -85,7 +183,7 @@ window.onscroll = function() {
     let new_y = Math.min(scrollPosition, scrollLimit);
     
     d3.select("#info-agent")
-      .select("rect")
-      .attr('y', new_y);
+    .attr('transform', `translate(0, ${new_y})`);
+    
   }
   
